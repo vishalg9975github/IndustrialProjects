8b5115fd74e7f24f6f16ae54344bcb06a3bccf23
Madhurimagaikwad <madhurimag1996@gmail.com>
2023-05-12 09:52:21 +0530

    get and update Reimbursement

--- EcommerceProject/src/main/java/com/velocity/service/ReimbursementService.java
+++ EcommerceProject/src/main/java/com/velocity/service/ReimbursementService.java
@@ -5,4 +5,6 @@ import com.velocity.model.Reimbursement;
 public interface ReimbursementService {
 
 	public Reimbursement saveReimbursement(Reimbursement reimbursement);
+	public Reimbursement updateReimbursement(Reimbursement reimbursement);
+	public Reimbursement getReimbursement();
 }
