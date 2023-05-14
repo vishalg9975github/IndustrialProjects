e2f0a1b5384a38684a83d701e76cbbd3b73eafb3
vishalg9975github <130144958+vishalg9975github@users.noreply.github.com>
2023-05-14 15:01:14 +0530

    Update ReimbursementService.java

--- EcommerceProject/src/main/java/com/velocity/service/ReimbursementService.java
+++ EcommerceProject/src/main/java/com/velocity/service/ReimbursementService.java
@@ -8,9 +8,7 @@ public interface ReimbursementService {
 
 	public Reimbursement saveReimbursement(Reimbursement reimbursement);
 	public Reimbursement updateReimbursement(Reimbursement reimbursement);
-<<<<<<< HEAD
 	public List<Reimbursement> getReimbursement();
-=======
 	public Reimbursement getReimbursement();
->>>>>>> eda9ac4b3ad152805826954ae780c1de790c97ee
+
 }
